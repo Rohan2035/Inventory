39bc9d725fbd05efbade14e7864c799ac7e0fd11
Rohan2035 <rohanvenkateshgupta.12@gmail.com>
2026-07-06 22:00:52 +0530

    Microservice Migration - Created orders module

---
+++ open-ecom-orders/src/test/java/com/openecom/orders/OpenEcomOrdersTest.java
@@ -0,0 +1,14 @@
+package com.openecom.orders;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OpenEcomOrdersTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
